2bc64a713357e2914b05396b0dc6e1acb593a867
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-04-02 22:52:29 +0530

    Update OfficeInfo.js

--- src/Components/OfficeInfo/OfficeInfo.js
+++ src/Components/OfficeInfo/OfficeInfo.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Iframe from 'react-iframe'
-import { BrowserRouter, Route, Link } from "react-router-dom";
 import './OfficeInfo.css'
 
 function OfficeInfo() {
