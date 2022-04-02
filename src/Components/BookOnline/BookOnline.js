be57adbd61f24c5ac5d4a9a18d22c501d5946db8
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-04-02 23:01:42 +0530

    Update BookOnline.js

--- src/Components/BookOnline/BookOnline.js
+++ src/Components/BookOnline/BookOnline.js
@@ -13,12 +13,12 @@ function BookOnline() {
                </div>
                   <Row>
                     <Col md={6} sm={6}>
-                       <Form.Label for="name">Name</Form.Label>
+                       <Form.Label htmlFor="name">Name</Form.Label>
                         <Form.Control type="text" className="form-control" name="name" placeholder="Full Name" />
                         </Col>
 
                                    <Col md={6} sm={6}>
-                                   <Form.Label for="select">Select Age</Form.Label>
+                                   <Form.Label htmlFor="select">Select Age</Form.Label>
                                         <Form.Select className="form-control" name="department">
                                              <option>0-18</option>
                                              <option>18-30</option>
@@ -29,18 +29,18 @@ function BookOnline() {
                                     </Row>
                                     <Row>
                                      <Col md={6} sm={6}>
-                                     <Form.Label for="telephone">Select Time</Form.Label>
+                                     <Form.Label htmlFor="telephone">Select Time</Form.Label>
                                         <Form.Control type="time" className="form-control" name="time" placeholder="Time"/>
                                      </Col>
                                      <Col md={6} sm={6}>
-                                        <Form.Label for="date">Select Date</Form.Label>
+                                        <Form.Label htmlFor="date">Select Date</Form.Label>
                                         <Form.Control type="date" name="date" className="form-control" />
                                    </Col>
                                     </Row>
                                    <Col md={12} sm={12}>
-                                        <Form.Label for="telephone">Phone Number</Form.Label>
+                                        <Form.Label htmlFor="telephone">Phone Number</Form.Label>
                                         <Form.Control type="tel" className="form-control" name="p_number" placeholder="Phone"/>
-                                        <Form.Label for="Message">Decribe Problem:</Form.Label>
+                                        <Form.Label htmlFor="Message">Decribe Problem:</Form.Label>
                                         <Form.Control as="textarea" className="form-control mb-3" rows="5" name="message" placeholder="You can describe your problem here..."></Form.Control>
                                    </Col>
                               <Row>
