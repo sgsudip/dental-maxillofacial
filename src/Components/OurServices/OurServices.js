f3774dad9abdd56a4408149a14676ab26fcf0d03
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-04-02 23:04:21 +0530

    Update OurServices.js

--- src/Components/OurServices/OurServices.js
+++ src/Components/OurServices/OurServices.js
@@ -10,78 +10,78 @@ function OurServices() {
             <h1 className="text-center mt-3">SPECIALITY TREATMENT FOR</h1>
             <Row>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/implant.webp' />
-                    <div class="overlay top-overlay">
-                    <div class="text">DENTAL IMPLANTS</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">DENTAL IMPLANTS</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/surgery.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">WISDOM TOOTH SURGERY</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">WISDOM TOOTH SURGERY</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/fractures.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">FRACTURES OF JAW & FACE</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">FRACTURES OF JAW & FACE</div>
                     </div>
                 </div>
                 </Col>
             </Row>
             <Row>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/crown.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">CAD/CAM CROWN AND BRIDGE</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">CAD/CAM CROWN AND BRIDGE</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/denture.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">COMPLETE & PARTIAL DENTURE</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">COMPLETE & PARTIAL DENTURE</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/rootcanal.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">ROOT CANAL TREATMENT</div>
+                    <div cclassName="overlay top-overlay">
+                    <div className="text">ROOT CANAL TREATMENT</div>
                     </div>
                 </div>
                 </Col>
             </Row>
             <Row>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/ortho.webp' />
-                    <div class="overlay top-overlay">
-                    <div class="text">ORTHODONTIC TREATMENT</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">ORTHODONTIC TREATMENT</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/smile.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">SMILE DESIGN/FULL MOUTH REHABILITATION</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">SMILE DESIGN/FULL MOUTH REHABILITATION</div>
                     </div>
                 </div>
                 </Col>
                 <Col md={6} lg={4}>
-                <div class="item animated wow fadeIn">
+                <div className="item animated wow fadeIn">
                     <Image src='images/laser.jpg' />
-                    <div class="overlay top-overlay">
-                    <div class="text">DENTAL LASER/BLEEDING & SWOLLEN GUMS</div>
+                    <div className="overlay top-overlay">
+                    <div className="text">DENTAL LASER/BLEEDING & SWOLLEN GUMS</div>
                     </div>
                 </div>
                 </Col>
